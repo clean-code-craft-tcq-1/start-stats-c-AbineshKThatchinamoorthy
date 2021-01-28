dd475f5018acecd952718e3885ee970a0b7bd503
AbineshKThatchinamoorthy <77613419+AbineshKThatchinamoorthy@users.noreply.github.com>
2021-01-28 17:44:36 +0530

    Update stats.h
    Error Fix 2

--- stats.h
+++ stats.h
@@ -1,11 +1,4 @@
-typedef struct
-{
-	int average = 0;
-	int min = 0;
-	int max = 0;
-}Stats;
-
-struct Stats compute_statistics(const float* numberset, int setlength);
+typedef struct Stats compute_statistics(const float* numberset, int setlength);
 
 typedef void (*alerter_funcptr)();
 void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats);
