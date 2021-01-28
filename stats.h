efda722d917aeaba242568dd81f779a5cce0a6b1
AbineshKThatchinamoorthy <77613419+AbineshKThatchinamoorthy@users.noreply.github.com>
2021-01-28 21:11:56 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -1,14 +1,17 @@
-typedef struct
+#include <stdlib.h>
+
+/*structure for stats*/
+struct Stats 
 {
-	int average = 0;
-	int min = 0;
-	int max = 0;
-}Stats;
+ float average; 
+ float min;
+ float max;
+};
 
-Stats compute_statistics(float* numberset, int setlength);
-
+struct Stats compute_statistics(const float* numberset, int setlength);
 typedef void (*alerter_funcptr)();
-void check_and_alert(float maxThreshold, alerter_funcptr alerters[], Stats computedStats);
+void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats);
 
 extern int emailAlertCallCount;
 extern int ledAlertCallCount;
+© 2021 GitHub, Inc.
