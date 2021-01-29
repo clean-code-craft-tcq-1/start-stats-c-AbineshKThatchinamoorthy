11ad4eb52192f0fecd2cd98b9b6d476eeb753b89
AbineshKThatchinamoorthy <77613419+AbineshKThatchinamoorthy@users.noreply.github.com>
2021-01-29 14:07:52 +0530

    Update stats-test.cpp

--- stats-test.cpp
+++ stats-test.cpp
@@ -18,13 +18,8 @@ TEST_CASE("reports average, minimum and maximum") {
 
 TEST_CASE("average is NaN for empty array") {
     Stats computedStats_s = compute_statistics_s(0, 0);
-    //All fields of computedStats (average, max, min) must be
-    //NAN (not-a-number), as defined in math.h
-    
-    //Use https://stackoverflow.com/questions/1923837/how-to-use-nan-and-inf-in-c
-    REQUIRE(computedStats_s.average == NaN);
-    //REQUIRE(computedStats_s.max     == NAN);
-    //REQUIRE(computedStats_s.min     == NAN);
+
+    REQUIRE(1 == isnan(computedStats.average));
 }
 
 TEST_CASE("raises alerts when max is greater than threshold") {
