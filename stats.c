6bdc76d9364289546f54d499b0d2d1aecfa827a7
AbineshKThatchinamoorthy <77613419+AbineshKThatchinamoorthy@users.noreply.github.com>
2021-01-29 10:59:53 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -1,12 +1,31 @@
 #include "stats.h"
 
 struct Stats compute_statistics(const float* numberset, int setlength) {
-    struct Stats s;
-    s.average = 0;
-    s.min = 0;
-    s.max = 0;
     
-    return s;
+  float sum_f = 0.0; 
+  int count_i;
+  struct Stats statistics_s;
+  statistics_s.min = numberset[0];
+  statistics_s.max = numberset[0];
+    
+  /*calculating sum*/
+  for (count=0 ;count<setlength;count++)  
+  {
+      sum +=numberset[count];
+      if (statistics_s.min > numberset[count])
+      {
+         statistics_s.min = numberset[count];
+      }
+      if (statistics_s.max < numberset[count])
+      {
+          statistics_s.max = numberset[count];
+      }
+  }
+    
+  /*calculating avg*/  
+  statistics_s.average = (sum_f/setlength);
+    
+    return statistics_s;
 }
 
 
