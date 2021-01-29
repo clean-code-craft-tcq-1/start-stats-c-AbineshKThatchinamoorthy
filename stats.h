34bd908869db611566539d2f3dc4ba98182f1290
AbineshKThatchinamoorthy <77613419+AbineshKThatchinamoorthy@users.noreply.github.com>
2021-01-29 16:22:06 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -9,14 +9,5 @@ struct Stats
  float max;
 };
 
-typedef void (*alerter_funcptr)();
-
 /* FUNCTION DECLARATION */
 struct Stats compute_statistics_s(const float* numberset_a, int setlength_i);
-void check_and_alert_v(float maxThreshold_f, alerter_funcptr alerters[], struct Stats computedStats_s);
-void emailAlerter_v();
-void ledAlerter_v();
-
-/* EXTERN VARIABLES */
-extern int emailAlertCallCount_i;
-extern int ledAlertCallCount_i;
