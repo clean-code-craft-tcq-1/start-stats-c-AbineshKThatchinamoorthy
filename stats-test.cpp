ed83266b37bc82f531a903fdba29b18ede6c3e4d
AbineshKThatchinamoorthy <77613419+AbineshKThatchinamoorthy@users.noreply.github.com>
2021-01-29 16:04:18 +0530

    Update stats-test.cpp

--- stats-test.cpp
+++ stats-test.cpp
@@ -17,7 +17,7 @@ TEST_CASE("reports average, minimum and maximum") {
 }
 
 TEST_CASE("average is NaN for empty array") {
- 	struct Stats computedStats_s = compute_statistics_s(0, 0); 	
+ 	//struct Stats computedStats_s = compute_statistics_s(0, 0); 	
  }
 
 TEST_CASE("raises alerts when max is greater than threshold") {
